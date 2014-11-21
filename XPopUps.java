7e79ce30a5d2bd53f0d5d867128975b4b6f78911
nzetzl <nzetzl@purdue.edu>
2014-11-21 15:01:50 -0500

    added source code files

---
+++ XPopUps.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.awt.Component;
+
+
+public class XPopUps extends JDialog {
+    
+    public static void main(String[] args) {
+        JOptionPane popUp = new JOptionPane("You need to Upgrade your software!");
+        Icon icon = UIManager.getIcon("OptionPane.warningIcon");
+        popUp.setIcon(icon);
+        JDialog d = popUp.createDialog(null, "You Got Hacked!");
+        d.setLocationRelativeTo(null);
+        while(true) {
+            for (int i = 0; i < 8; i++) {
+                d.setLocation(150 * i, 100 * i);
+                d.setVisible(true);
+            }
+            for (int i = 0; i < 8; i++) {
+                d.setLocation(150 * i, 750 - 125 * i);
+                d.setVisible(true);
+            }
+        }
+    }
+}
